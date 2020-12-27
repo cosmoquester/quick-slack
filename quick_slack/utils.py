248f4069d04d97d0ea39e027c2f6f37907596d20
박상준 <psj8252@naver.com>
2020-12-27 16:10:40 +0900

    feat: Add function to get channel id by name

--- quick_slack/utils.py
+++ quick_slack/utils.py
@@ -18,7 +18,7 @@ def modify_config(key, value):
     config[key] = value
 
     with open(CONFIG_FILE_PATH, "w") as f:
-        json.dump(config, f)
+        json.dump(config, f, indent=4)
 
 
 def send_message(token, channel_id, text):
@@ -29,3 +29,24 @@ def send_message(token, channel_id, text):
     ).json()
 
     return response
+
+
+def get_channel_id(channel_name):
+    config = load_config()
+
+    uri = "https://slack.com/api/conversations.list"
+    pararms = {"types": "public_channel,private_channel"}
+    prev_id_map = {}
+    while True:
+        response = requests.get(
+            uri, params=pararms, headers={"Authorization": f"Bearer {config['slack_oauth_token']}"}
+        ).json()
+        id_map = {channel_info["name"]: channel_info["id"] for channel_info in response["channels"]}
+        cursor = response["response_metadata"]["next_cursor"]
+        pararms["cursor"] = cursor
+
+        if channel_name in id_map:
+            return id_map[channel_name]
+        elif len(prev_id_map) == len({**prev_id_map, **id_map}):
+            return None
+        prev_id_map = id_map
