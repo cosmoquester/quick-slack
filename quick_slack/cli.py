764ac47468bb6b4ed066bb542f8a14fefaa6690d
박상준 <psj8252@naver.com>
2020-12-27 17:13:10 +0900

    refactor: Set config command

--- quick_slack/cli.py
+++ quick_slack/cli.py
@@ -1,5 +1,6 @@
 import click
 
+from .low_api import get_channel_id, get_direct_message_id
 from .utils import load_config, modify_config
 
 config = click.Group()
@@ -12,17 +13,30 @@ def show():
         click.echo(f"{key:20s}: {str(value) if value else 'None'}")
 
 
-@config.command("set", help="Set config")
+@config.command("set")
 @click.option("--api-token", help="Slack oauth API token start with xoxb-...")
 @click.option("--default-mention", help="Default mention users")
-@click.option("--default-channel", help="Default channel to send message")
-def set_config(api_token, default_mention, default_channel):
-    configs = {}
+@click.option(
+    "--default-channel-name",
+    help="Default channel name to send message, enter '@username' if channel is direct message",
+)
+@click.option("--default-channel-id", help="Default channel id to send message")
+def set_config(api_token, default_mention, default_channel_name, default_channel_id):
+    """
+    Set configurations
+
+    Default channel is always only one, so you cannot and don't need to pass default channel name and default channel id.
+    """
+    if default_channel_name and default_channel_id:
+        click.echo("Cannot pass default-channel-name and default-channel-id! default-channel is only one!", err=True)
+        exit(1)
+
     if api_token:
         if not api_token.startswith("xoxb-"):
             click.echo("Slack oauth token is invalid! The token should start with xoxb-...", err=True)
             exit(1)
-        configs["slack_oauth_token"] = api_token
+        modify_config("slack_oauth_token", api_token)
+        click.echo("Setting slack token is done.")
 
     if default_mention:
         default_mention = default_mention.split()
@@ -30,18 +44,21 @@ def set_config(api_token, default_mention, default_channel):
             if mention_user[0] != "@":
                 click.echo("Default mention format is like '@user1 @user2 @user3'. something invalid!", err=True)
                 exit(1)
-        configs["default_mentions"] = default_mention
+        modify_config("default_mentions", default_mention)
+        click.echo("Setting default mentions is done.")
 
-    if default_channel:
-        configs["default_channel_id"] = default_channel
+    if default_channel_name:
+        if default_channel_name[0] == "@":
+            default_channel_id = get_direct_message_id(default_channel_name[1:])
+        else:
+            default_channel_id = get_channel_id(default_channel_name)
+        if not default_channel_id:
+            click.echo("Channel name is not valid!", err=True)
+            exit(1)
 
-    if not configs:
-        click.echo("Please input parameter and value!", err=True)
-        exit(1)
-
-    for key, value in configs.items():
-        modify_config(key, value)
-    click.echo("Done.")
+    if default_channel_id:
+        modify_config("default_channel_id", default_channel_id)
+        click.echo("Setting default channel is done.")
 
 
 @click.command()
