9c566413b83d1b22fedb572a5bc8e61c60a5e12b
박상준 <psj8252@naver.com>
2020-12-28 19:43:15 +0900

    build: Version 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="quick-slack",
-    version="1.0.1",
+    version="1.0.2",
     description="CLI tool to send message to mornitor status, notify process end, etc by slack.",
     long_description=long_description,
     long_description_content_type="text/markdown",
